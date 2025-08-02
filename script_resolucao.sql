8bcfc69ec3af3749995ffc2210c9ebc42feb257a
LázaroK <lazarolk23@hotmail.com>
2025-08-02 03:34:39 -0300

    feat: Adiciona consulta do desafio 3

--- script_resolucao.sql
+++ script_resolucao.sql
@@ -1,6 +1,8 @@
 -- DESAFIO 01 - Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM Filmes;
 
-
 -- DESAFIO 02 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
+
+-- DESAFIO 03 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'de volta para o futuro';
