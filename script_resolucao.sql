4c62949a1f1872d3ad13d8a842b2fa4955aece29
LázaroK <lazarolk23@hotmail.com>
2025-08-02 03:34:20 -0300

    feat: Adiciona consulta do desafio 2

--- script_resolucao.sql
+++ script_resolucao.sql
@@ -1,3 +1,6 @@
 -- DESAFIO 01 - Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM Filmes;
 
+
+-- DESAFIO 02 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
